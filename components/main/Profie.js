d9489b7913c1482fa2d248fd003eb98218fd4a90
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-09-21 01:20:49 +0300

    following system

--- components/main/Profie.js
+++ components/main/Profie.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, Text, Image, FlatList } from 'react-native'
+import { StyleSheet, View, Text, Image, FlatList, TouchableOpacity, Button } from 'react-native'
 import { connect } from 'react-redux'
 import firebase from 'firebase'
+
 require('firebase/firebase-firestore')
 
-function Profie(props) {
-    const [userPosts, setUserPosts] = useState([])
-    const [user, setUser] = useState(null)
+function Profile(props) {
+    const [userPosts, setUserPosts] = useState([]);
+    const [user, setUser] = useState(null);
+    const [following, setFollowing] = useState(false);
 
     useEffect(() => {
         const { currentUser, posts } = props;
-        console.log({ currentUser, posts })
 
         if (props.route.params.uid === firebase.auth().currentUser.uid) {
-            setUser(currentUser);
-            setUserPosts(posts);
-        } else {
+            setUser(currentUser)
+            setUserPosts(posts)
+        }
+        else {
             firebase.firestore()
                 .collection("users")
                 .doc(props.route.params.uid)
@@ -40,37 +42,83 @@ function Profie(props) {
                         const id = doc.id;
                         return { id, ...data }
                     })
-                    setUserPosts(posts);
+                    setUserPosts(posts)
                 })
-
         }
-    }, [props.route.params.uid]);
+
+        if (props.following.indexOf(props.route.params.uid) > -1) {
+            setFollowing(true);
+        } else {
+            setFollowing(false);
+        }
+    }, [props.route.params.uid, props.following]);
+
+    const onFollow = () => {
+        firebase.firestore()
+            .collection("following")
+            .doc(firebase.auth().currentUser.uid)
+            .collection("userFollowing")
+            .doc(props.route.params.uid)
+            .set({})
+    }
+    const onUnfollow = () => {
+        firebase.firestore()
+            .collection("following")
+            .doc(firebase.auth().currentUser.uid)
+            .collection("userFollowing")
+            .doc(props.route.params.uid)
+            .delete()
+    }
 
     if (user == null) {
         return <View />
     }
 
     return (
-        <View styles={styles.container}>
-            <View styles={styles.containerInfo}>
+        <View style={styles.container}>
+            <View style={styles.containerInfo}>
                 <Text>{user.name}</Text>
                 <Text>{user.email}</Text>
+
+                {props.route.params.uid !== firebase.auth().currentUser.uid ? (
+                    <View>
+                        {following ? (
+                            <Button
+                                title="Following"
+                                onPress={() => onUnfollow()}
+                            />
+                        ) :
+                            (
+                                <Button
+                                    title="Follow"
+                                    onPress={() => onFollow()}
+                                />
+                            )}
+                    </View>
+                ) :
+                    <Button
+                        title="Logout"
+                        onPress={() => onLogout()}
+                    />}
             </View>
-            <View styles={styles.containerGallery}>
+
+            <View style={styles.containerGallery}>
                 <FlatList
                     numColumns={3}
                     horizontal={false}
                     data={userPosts}
                     renderItem={({ item }) => (
                         <View
-                            style={styles.containerImage}
-                        >
+                            style={styles.containerImage}>
+
                             <Image
                                 style={styles.image}
                                 source={{ uri: item.downloadURL }}
                             />
                         </View>
+
                     )}
+
                 />
             </View>
         </View>
@@ -99,6 +147,6 @@ const styles = StyleSheet.create({
 const mapStateToProps = (store) => ({
     currentUser: store.userState.currentUser,
     posts: store.userState.posts,
+    following: store.userState.following
 })
-
-export default connect(mapStateToProps, null)(Profie);
+export default connect(mapStateToProps, null)(Profile);
